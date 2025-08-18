30b867b010fce850707b41e27177df03e8c729bc
Muzdalifah20 <muzdalifahsuliman@gmail.com>
2025-08-18 14:47:20 +0300

    Update discount.py

--- plp-python/discount.py
+++ plp-python/discount.py
@@ -1,1 +1,16 @@
+def  calculate_discount(price, discount_percent):
+  if discount_percent >= 20:
+    total = price  (1 - discount_percent / 100)
+  else:
+    total = price
+  return total
 
+price = float(input("Enter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+final_price = calculate_discount(price, discount_percent)
+
+if discount_percent:
+  print(f"Final price after {discount_percent}% discount: {final_price:.2f")
+else:
+  print(f"No discount applied. The original price is {price:.f2}")
